558d63f379592d4a0e0e959b76920a63bc37bf99
Jie Lu <lujie1996@hotmail.com>
2019-11-30 22:33:28 -0800

    add static folder; add generate_files.py

---
+++ examples/fileserver/generate_files.py
@@ -0,0 +1,21 @@
+import os
+
+if __name__ == '__main_':
+
+	# n = input('Number of files: ')
+	# s = input('File size (MB): ')
+	# n = int(n)
+	# s = int(s)
+	n = 100
+	s = 5
+
+	path = 'static/'
+	filename_prefix = str(s) + 'mb_'
+	for i in range(n):
+		total_path = path + filename_prefix + str(i)
+	    f = open(total_path, 'wb')
+	    f.seek(s * 1048576)
+		f.write(b'\0')
+		f.close()
+
+	print 'created!'
