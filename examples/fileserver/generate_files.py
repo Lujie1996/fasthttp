107dd9f8e4fefd50d50fd199af3fed60e3f8105d
Jie Lu <lujie1996@hotmail.com>
2019-12-03 19:14:43 -0800

    modify generate

--- examples/fileserver/generate_files.py
+++ examples/fileserver/generate_files.py
@@ -1,4 +1,19 @@
 import os
+import random
+import string
+
+def generate_big_random_letters(filename,size):
+    """
+    generate big random letters/alphabets to a file
+    :param filename: the filename
+    :param size: the size in bytes
+    :return: void
+    """
+    chars = ''.join([random.choice(string.letters) for i in range(size)]) #1
+
+    with open(filename, 'w') as f:
+        f.write(chars)
+    pass
 
 if __name__ == '__main__':
 	print 'start generating random files...'	
@@ -7,17 +22,16 @@ if __name__ == '__main__':
 	# s = input('File size (MB): ')
 	# n = int(n)
 	# s = int(s)
-	n = 100
-	s = 5
+	n = 10
+	s = 1
+	size_in_byte = s * 1024 * 1024
 
 	path = 'static/'
 	os.mkdir(path, 0755);
+
 	filename_prefix = str(s) + 'mb_'
 	for i in range(n):
-		total_path = path + filename_prefix + str(i)
-		f = open(total_path, 'wb')
-		f.seek(s * 1048576)
-		f.write(b'\0')
-		f.close()
+		total_path = path + filename_prefix + str(i) + ".txt"
+		generate_big_random_letters(total_path, size_in_byte)
 		print str(i + 1) + ' / ' + str(n) + ' files created..' 
 	print 'Random file generation finished.'
